1c64708ebc67a902036bce9d3c9887f4c088aafe
wangxiaoshu <wangxiaoshu@yidian-inc.com>
2016-09-11 14:47:09 +0800

    add hosts

--- run_mpi_ftrl.sh
+++ run_mpi_ftrl.sh
@@ -7,5 +7,5 @@ do
 done
 scp train worker@10.101.2.89:/home/worker/xiaoshu/DML/logistic_regression_mpi/.
 scp train worker@10.101.2.90:/home/worker/xiaoshu/DML/logistic_regression_mpi/.
-mpirun -f ../hosts -np $process_number ./train ftrl 50 5000 ./data/train ./data/test
+mpirun -f ./hosts -np $process_number ./train ftrl 50 5000 ./data/train ./data/test
 #mpirun -f ../hosts -np $process_number ./train ftrl ./data/agaricus.txt.train ./data/agaricus.txt.test
